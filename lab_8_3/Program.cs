e7127f45d603bba491fdeb133663675358d00526
Elizaveta Pernikova <lisapernikowa@mail.ru>
2022-12-04 23:23:25 +0500

    парирумчик парирум

--- lab_8_3/Program.cs
+++ lab_8_3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Immutable;
+using System.Runtime.InteropServices;
 
 namespace lab8_3
 {
@@ -9,14 +10,16 @@ namespace lab8_3
         {
             string[] array = { "code", "doce", "ecod", "framer", "frame" };
             List<string> list = new List<string>();
-
+            Console.Write("Array before: ");
             foreach (string item in array)
+            {
                 list.Add(item);
+                Console.Write($"{item} ");
+            }
 
             for(int i =0; i < array.Length; i++)
             {
                 char[] arr1 = array[i].ToCharArray();
-                string str = "";
                 for(int j = i+1; j < array.Length; j++)
                 {
                     char[] arr2 = array[j].ToCharArray();
@@ -25,11 +28,31 @@ namespace lab8_3
                 }
             }
             
+            Console.Write($"\nArray after: {String.Join(" ", list)}");
         }
         static bool EquivalenceCheck(char[] arr1, char[] arr2)
         {
-            bool fl = false;
+            if (arr1.Length != arr2.Length)
+                return false;
 
+            char[] arr = new char[arr1.Length];
+            for(int i = 0; i < arr1.Length; i++)
+            {
+                for(int j = 0; j < arr2.Length; j++)
+                {
+                    if (arr1[i] == arr2[j])
+                        arr[i] = arr2[j];
+                }
+            }
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == arr1[i])
+                    continue;
+                else
+                    return false;
+            }
+            return true;
+            ///return string.Concat(s).Equals(string.Concat(m));
         }
     }
 }
