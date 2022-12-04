f841a41f63c18eb7fac844438ad245f6f9b40d6a
Elizaveta Pernikova <lisapernikowa@mail.ru>
2022-12-04 22:48:26 +0500

    dd

--- lab_8_3/Program.cs
+++ lab_8_3/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿using System;
+using System.Collections.Immutable;
+
+namespace lab8_3
+{
+    class Programm
+    {
+        static void Main()
+        {
+            string[] array = { "code", "doce", "ecod", "framer", "frame" };
+            List<string> list = new List<string>();
+
+            foreach (string item in array)
+                list.Add(item);
+
+            for(int i =0; i < array.Length; i++)
+            {
+                char[] arr1 = array[i].ToCharArray();
+                string str = "";
+                for(int j = i+1; j < array.Length; j++)
+                {
+                    char[] arr2 = array[j].ToCharArray();
+                    if (EquivalenceCheck(arr1, arr2))
+                        list.Remove(array[j]);
+                }
+            }
+            
+        }
+        static bool EquivalenceCheck(char[] arr1, char[] arr2)
+        {
+            bool fl = false;
+
+        }
+    }
+}
